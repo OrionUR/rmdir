5eb6ae4b5b0e5b87482e8095ab35be5e107b2f4d
orion_ou <orion_ou@hotmail.com>
2019-09-24 19:40:24 -0700

    add comments and change some error strings

--- rmdir_test.c
+++ rmdir_test.c
@@ -8,60 +8,127 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include "rmdir.h"
 
-#define ROOT_FOLDER                 "mkdir ./test_folder/"
-#define SUB_FOLDER      ROOT_FOLDER     "sub/"
-#define SSUB_FOLDER     SUB_FOLDER          "ssub/"
-#define SUB_FOLDER2     ROOT_FOLDER     "sub2/"
-#define SSUB_FOLDER2    SUB_FOLDER2         "ssub2/"
-#define FILEA               "touch ./test_folder/filea"
-#define FILEB               "touch ./test_folder/sub2/fileb.txt"
-#define DELETE_TEST_FOLDER       "rm -rf ./test/"
-#define ERROR_FILE          "./test_folder/abcde"
-#define ERROR_FOLDER        "./abcde/"
-#define CORRECT_FILE        "./test_folder//sub2/fileb.txt"
-#define CORRECT_FOLDER      "./test_folder/"
+// define tree of test folders and files
+#define ROOT_FOLDER                 "./test_folder/"
+#define SUB_FOLDER      ROOT_FOLDER         "sub/"
+#define SSUB_FOLDER     SUB_FOLDER              "ssub/"
+#define SUB_FOLDER2     ROOT_FOLDER         "sub2/"
+#define SSUB_FOLDER2    SUB_FOLDER2             "ssub2/"
+#define EMPTY_FILE      ROOT_FOLDER         "empty_file"
+#define CONTENT_FILE    SUB_FOLDER2             "content_file.txt"
+// define error folder and file
+#define ERROR_FILE      ROOT_FOLDER         "abcde"
+#define ERROR_FOLDER                "./abcde/"
 
 #define MAXIMUM_TEST_STRING_LENGTH      4096
 #define MAXIMUM_STRING_LENGTH   MAXIMUM_TEST_STRING_LENGTH+1
 
-int initial_test()
+/** create folder
+ * 
+ * @param: const char *path:
+ *                  the path of folder which is going to create
+ * @return: bool:
+ *                  true for success, false for failure
+**/
+bool create_folder(const char *path)
+{
+    const char  *create_folder_command = "mkdir";
+    char execute_command[MAXIMUM_STRING_LENGTH];
+
+    strncpy(execute_command, create_folder_command, 
+            sizeof(create_folder_command));
+    strncat(execute_command, " ", 1);
+    strncat(execute_command, path, strlen(path));
+    if (0 != system(execute_command))
+    {
+        return false;
+    }
+
+    return true;
+}
+
+/** create file
+ * 
+ * @param: const char *path:
+ *                  the path of file which is going to create
+ * @return: bool:
+ *                  true for success, false for failure
+**/
+bool create_file(const char *path)
+{
+    const char  *create_file_command = "touch";
+    char execute_command[MAXIMUM_STRING_LENGTH];
+
+    strncpy(execute_command, create_file_command, 
+            sizeof(create_file_command));
+    strncat(execute_command, " ", 1);
+    strncat(execute_command, path, strlen(path));
+    if (0 != system(execute_command))
+    {
+        return false;
+    }
+
+    return true;
+}
+
+/** initial the test of rmdir
+ *  it will create some folders and files and make it to be a tree
+ * 
+ * @return: bool:
+ *                  true for success, false for failure
+**/
+bool initial_test()
 {
     FILE *fp;
 
-    if ((0 != system(ROOT_FOLDER))
-    ||  (0 != system(SUB_FOLDER))
-    ||  (0 != system(SSUB_FOLDER))
-    ||  (0 != system(SUB_FOLDER2))
-    ||  (0 != system(SSUB_FOLDER2))
-    ||  (0 != system(FILEA))
-    ||  (0 != system(FILEB)))
+    if (!(create_folder(ROOT_FOLDER))
+    ||  !(create_folder(SUB_FOLDER))
+    ||  !(create_folder(SSUB_FOLDER))
+    ||  !(create_folder(SUB_FOLDER2))
+    ||  !(create_folder(SSUB_FOLDER2))
+    ||  !(create_file(EMPTY_FILE))
+    ||  !(create_file(CONTENT_FILE)))
     {
-        return -1;
+        return false;
     }
 
-    if (NULL == (fp = fopen("./test_folder/sub2/fileb.txt", "w")))
+    if (NULL == (fp = fopen(CONTENT_FILE, "w")))
     {
-        return -1;
+        return false;
     }
     fputs("This is text file with content", fp);
     if (0 != fclose(fp))
     {
-        return -1;
+        return false;
     }
 
-    return 0;
+    return true;
 }
 
-int end_test()
+/** end the test of rmdir
+ *  it will delete all the folders and files when test is failure
+ * 
+ * @return: bool:
+ *                  true for success, false for failure
+**/
+bool end_test()
 {
-    if (0 != system(DELETE_TEST_FOLDER))
+    const char *delete_folder_command = "rm -rf";
+    char execute_command[MAXIMUM_STRING_LENGTH];
+    
+    strncpy(execute_command, delete_folder_command, 
+            sizeof(delete_folder_command));
+    strncat(execute_command, " ", 1);
+    strncat(execute_command, ROOT_FOLDER, sizeof(ROOT_FOLDER));
+    if (0 != system(execute_command))
     {
-        return -1;
+        return false;
     }
 
-    return 0;
+    return true;
 }
 
 int main()
@@ -72,7 +139,7 @@ int main()
 
     // initialize
     printf("initializing...\n");
-    if (0 != initial_test())
+    if (!(initial_test()))
     {
         printf("initialize error\n");
         return -1;
@@ -133,36 +200,54 @@ int main()
     }
     printf("path is error folder name......ok\n\n");
 
-    // test path is correct file name
-    printf("testing path is correct file name...\n");
-    if (EDEL_OK != delete_folder(CORRECT_FILE))
+    // test delete a empty file
+    printf("testing delete a empty file...\n");
+    if (EDEL_OK != delete_folder(EMPTY_FILE))
     {
         printf("test error\n");
         goto error_occur;
     }
-    printf("path is correct file name......ok\n\n");
+    printf("delete a empty file......ok\n\n");
 
-    // test path is correct folder name
-    printf("testing path is correct folder name...\n");
-    if (EDEL_OK != delete_folder(CORRECT_FOLDER))
+    // test delete a file with content
+    printf("testing delete a file with content...\n");
+    if (EDEL_OK != delete_folder(CONTENT_FILE))
     {
         printf("test error\n");
         goto error_occur;
     }
-    printf("path is correct folder name......ok\n\n");
+    printf("delete a file with content......ok\n\n");
+
+    // test delete a empty folder
+    printf("testing delete a empty folder...\n");
+    if (EDEL_OK != delete_folder(SSUB_FOLDER))
+    {
+        printf("test error\n");
+        goto error_occur;
+    }
+    printf("delete a empty folder......ok\n\n");
+
+    // test delete the test folder
+    printf("testing delete the test folder...\n");
+    if (EDEL_OK != delete_folder(ROOT_FOLDER))
+    {
+        printf("test error\n");
+        goto error_occur;
+    }
+    printf("delete the test folder......ok\n\n");
     printf("Done!!!\n");
 
     return 0;
 
 // if error occur, then delete test folder
 error_occur:
-    if (0 != end_test())
+    if (end_test())
     {
-        printf("remove test folder error\n");
+        printf("remove test folder......ok\n");
     }
     else
     {
-        printf("remove test folder......ok\n\n");
+        printf("remove test folder error\n");
     }
 
     return -1;
